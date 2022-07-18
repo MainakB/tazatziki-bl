6aaeeed58713d7ebff1fbbbbd21911f2858f8ace
Mainak Basu <kaniam@Mainaks-MacBook-Pro.local>
2022-07-18 00:06:14 -0400

    test

---
+++ src/services/login-service/locators/login-locators.ts
@@ -0,0 +1,41 @@
+import { Types } from "@kaniamb/tzatziki-core";
+
+export const common_PO: Types.ILocatorMetadataObject = {
+  user: {
+    description: "user id",
+    locator: [
+      {
+        locatorType: Types.LocatorTypes.ID,
+        locatorValue: "replaceText",
+      },
+    ],
+  },
+  password: {
+    description: "password",
+    locator: [{ locatorType: Types.LocatorTypes.ID, locatorValue: "password" }],
+  },
+};
+
+export const user_preference_po: Types.ILocatorMetadataObject = {
+  submit: {
+    description: "submit button",
+    locator: [
+      {
+        locatorType: Types.LocatorTypes.CSS,
+        locatorValue: "button[type='submity']",
+      },
+      {
+        locatorType: Types.LocatorTypes.XPATH,
+        locatorValue: ".//button[@type='submits']",
+      },
+      // {
+      //   locatorType: LocatorTypes.CSS,
+      //   locatorValue: ".ABCD",
+      // },
+      // {
+      //   locatorType: LocatorTypes.XPATH,
+      //   locatorValue: 'button[type="submitnow"]',
+      // },
+    ],
+  },
+};
